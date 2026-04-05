8ee120de59c7764c81db38b44f256b16a8e12caa
wachirachaisw2002-wq <wachirachai.sw2202@gmail.com>
2026-04-05 22:57:13 +0700

    update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth"; // 1. เพิ่มบรรทัดนี้
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyCl6cAGje1ylRDzHTf9BuHHPL-bmuErCUk",
+    authDomain: "career-path-db.firebaseapp.com",
+    projectId: "career-path-db",
+    storageBucket: "career-path-db.firebasestorage.app",
+    messagingSenderId: "443079461430",
+    appId: "1:443079461430:web:f68e081ca8f3fe113da697",
+    measurementId: "G-L8V65WCXQT"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app); 
+export const auth = getAuth(app); // 2. เพิ่มบรรทัดนี้เพื่อส่งออก auth ไปใช้
 export const storage = getStorage(app);
